fd0d948e9c3e331a77413282fb8a2d2c979f0687
IdaszakDaniel <idaszak1@gmail.com>
2020-05-20 18:26:15 +0200

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Daniel Idaszak on 20/05/2020.
+//  Copyright © 2020 Daniel Idaszak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
