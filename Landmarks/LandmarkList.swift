1884d0c370e50f06188bbab3873df839a2e8544f
IdaszakDaniel <idaszak1@gmail.com>
2020-05-21 07:13:39 +0200

    dynamic LandmarkList added

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Daniel Idaszak on 21/05/2020.
+//  Copyright © 2020 Daniel Idaszak. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarkData, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
