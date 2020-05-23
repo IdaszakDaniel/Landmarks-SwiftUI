b4947aa5b4cbbd099e278b4dded7b51d59856fde
IdaszakDaniel <idaszak1@gmail.com>
2020-05-23 14:09:01 +0200

    Badge View finished

---
+++ Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Daniel Idaszak on 23/05/2020.
+//  Copyright © 2020 Daniel Idaszak. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
